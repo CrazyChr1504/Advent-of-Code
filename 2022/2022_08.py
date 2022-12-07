684596e397b5f9765a7351cc50b058decc3d95c3
CrazyChr1504 <chris95504@gmail.com>
2022-12-07 14:05:17 +0100

    Folder Sorting

---
+++ 2022/2022_08.py
@@ -0,0 +1,10 @@
+def import_fie():
+    with open("Aoc_Inputs\Advent_08_22.txt") as f: pass
+
+def part_one(): pass
+
+def part_two(): pass
+
+if __name__ == "__main__":
+    part_one()
+    part_two()
