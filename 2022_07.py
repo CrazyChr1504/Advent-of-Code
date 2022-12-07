da6527eb4f09ccdf3c71a0aa100f399408975625
CrazyChr1504 <chris95504@gmail.com>
2022-12-07 12:38:28 +0100

    Day 7

---
+++ 2022_07.py
@@ -0,0 +1,32 @@
+cur_dir = []
+dirs = {}
+with open("Advent_07_22.txt","rt") as f:
+  for l in f:
+    e = l.strip().split()
+    if l.startswith("$ cd"):
+      if e[2] == "..":
+        cur_dir.pop()
+      else:
+        cur_dir.append(e[2])
+      p = '/'.join(cur_dir[:-1])
+      d = '/'.join(cur_dir)
+      if d not in dirs:
+        dirs[d] = [0,p]
+    elif e[0] not in ("$","dir"):
+      s = int(e[0])
+      d = '/'.join(cur_dir)
+      while d:
+        dirs[d][0] += s
+        d = dirs[d][1]
+  size = 0
+d = []
+for s,_ in dirs.values():
+  d.append(s)
+  if s <= 100_000:
+    size += s
+need = 30_000_000 - (70_000_000 - dirs["/"][0])
+for s in sorted(d):
+  if s >= need:
+    print(size)
+    print(s)
+    break
