499ca9ded69a429f93ede780c48f396ac2816cd5
CrazyChr1504 <chris95504@gmail.com>
2023-12-07 10:09:26 +0100

    Day 7 Completed and slighty cleaned

--- 2023/2023_07.py
+++ 2023/2023_07.py
@@ -7,15 +7,6 @@ with open(file_name, "r") as f:
 
 cards = list('AKQJT98765432')[::-1]
 
-def second_categorize_hand(hand):
-  max_value = None
-  for c in cards:
-    replaced_hand = hand.replace('J',c)
-    replaced_value = categorize_hand(replaced_hand)
-    if max_value is None or replaced_value > max_value:
-      max_value = replaced_value
-  return max_value
-
 def categorize_hand(hand):
   if len(set(hand)) == 1:
     return 7
@@ -36,43 +27,15 @@ def categorize_hand(hand):
     return 2
   return 1
 
-def tie_breaker(a,b):
-  values = {
-    "A": 15,
-    "K": 14,
-    "Q": 13,
-    "J": 12,
-    "T": 11
-  }
-
-  for i in range(len(a)):
-    aval = a[i]
-    bval = b[i]
-    if not aval.isdigit():
-      aval = values[aval]
-    else:
-      aval = int(aval)
-    if not bval.isdigit():
-      bval = values[bval]
-    else:
-      bval = int(bval)
-    if aval < bval:
-      return -1
-    elif aval > bval:
-      return 1
-  return 0
-
 def compare_hands(a,b):
     a,_ = a
     b,_ = b
     cha = categorize_hand(a)
     chb = categorize_hand(b)
-
     if cha > chb:
       return 1
     elif cha < chb:
       return -1
-
     for ca, cb in zip(a,b):
       if cards.index(ca) > cards.index(cb):
         return 1
@@ -83,7 +46,6 @@ def compare_hands(a,b):
 hands = [l.split() for l in data if len(l) > 0]
 def Part1():
   hands.sort(key=cmp_to_key(compare_hands))
-
   sum_wins = 0
   for i, (h, bid) in enumerate(hands):
     sum_wins += int(bid)*(i+1)
@@ -94,28 +56,33 @@ def second_compare_hands(a,b):
   b,_ = b
   cha = second_categorize_hand(a)
   chb = second_categorize_hand(b)
-
   if cha > chb:
     return 1
   elif cha < chb:
     return -1
-
   for ca, cb in zip(a,b):
     if cards.index(ca) > cards.index(cb):
       return 1
     elif cards.index(ca) < cards.index(cb):
       return -1
-
   return 0
+
 cards = list('AKQT98765432J')[::-1]
+
+def second_categorize_hand(hand):
+  max_value = None
+  for c in cards:
+    replaced_hand = hand.replace('J',c)
+    replaced_value = categorize_hand(replaced_hand)
+    if max_value is None or replaced_value > max_value:
+      max_value = replaced_value
+  return max_value
+
 def Part2():
   hands.sort(key=cmp_to_key(second_compare_hands))
-
   sum_wins = 0
-
   for i, (h, bid) in enumerate(hands):
     sum_wins += int(bid)*(i+1)
-
   print(sum_wins)
 
 def main():
