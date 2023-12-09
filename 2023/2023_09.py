d19ea5a5529afccc76d0f40a0753ec3fc1cade03
CrazyChr1504 <chris95504@gmail.com>
2023-12-09 11:05:05 +0100

    Day 9 Completed and cleaend up

---
+++ 2023/2023_09.py
@@ -0,0 +1,25 @@
+file_name = "AoC_Inputs\Advent_2023\Advent_09_23.txt"
+with open(file_name, "r") as f:
+	data = f.read()
+	nums = [list(map(int, lines.split())) for lines in data.splitlines()]
+
+def solve(nums):
+	if len(set(nums)) == 1:
+		return nums[0]
+	diff = []
+	for i in range(len(nums)-1):
+		diff.append(nums[i+1]-nums[i])
+	return nums[-1] + solve(diff)
+
+def Part1():
+	print(sum(solve(n) for n in nums))
+
+def Part2():
+	print(sum(solve(n[::-1]) for n in nums))
+
+def main():
+  Part1()
+  Part2()
+
+if __name__ == "__main__":
+  main()
